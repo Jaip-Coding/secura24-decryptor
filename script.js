860c618fe9f4f5bb53a8729045c0e8c300cb42fc
Jaip <145761147+Jaip-Coding@users.noreply.github.com>
2024-03-29 17:32:13 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,50 +1,47 @@
-// Secura24 Encryptor
+// Secura24 Decryptor
 
 function sortChars(list, char1, char2) {
-    let sort_elements = [list[char1], list[char2]];
-    [list[char1], list[char2]] = [sort_elements[1], sort_elements[0]];
+    const sort_elements = [list[char1], list[char2]];
+    [list[char2], list[char1]] = sort_elements;
     return list;
 }
 
 function encrypt() {
     const possible_chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!"§$%&/()=?+*#-_.:,;<>@ \n';
-    const key_chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!"§$%&/()=?+*#-_.:,;<>@';
-    let encrypted_chars = '';
-    
     let chars_to_use = possible_chars;
-    let key_list = [];
-    let sort_key = Math.floor(Math.random() * 5);
-    let key = '';
-    
-    for (let char of possible_chars) {
-        let random_char = Math.floor(Math.random() * chars_to_use.length);
-        encrypted_chars += chars_to_use[random_char];
-        chars_to_use = chars_to_use.slice(0, random_char) + chars_to_use.slice(random_char + 1);
-        key_list.push(random_char.toString());
+    let encrypted_chars = '';
+
+    const sort_key = parseInt(key.slice(-1));
+    let key_list = key.match(/\d+/g);
+    key_list.pop();
+
+    function sortChars(list, char1, char2) {
+      const sort_elements = [list[char1], list[char2]];
+      [list[char2], list[char1]] = sort_elements;
+      return list;
     }
-    
+
     let sort_number = 0;
     while (sort_number + sort_key < key_list.length) {
-        key_list = sortChars(key_list, sort_number, sort_number + sort_key);
-        sort_number += sort_key * 2;
+      sortChars(key_list, sort_number + sort_key, sort_number);
+      sort_number += sort_key * 2;
     }
-    
-    for (let item of key_list) {
-        key += item + key_chars[Math.floor(Math.random() * key_chars.length)];
+
+    for (let char of key_list) {
+      encrypted_chars += chars_to_use[parseInt(char)];
+      chars_to_use = chars_to_use.slice(0, parseInt(char)) + chars_to_use.slice(parseInt(char) + 1);
     }
-    key += sort_key;
-    
-    let user_input = document.getElementById('userInput').innerText;
-    let encrypted_result = '';
-    
+
+    let decrypted_result = '';
+
     for (let char of user_input) {
-        let char_index = possible_chars.indexOf(char);
-        if (char_index !== -1) {
-            encrypted_result += encrypted_chars[char_index];
-        }
+      const char_index = encrypted_chars.indexOf(char);
+      if (char_index !== -1) {
+        decrypted_result += possible_chars[char_index];
+      }
     }
-    
-    document.getElementById('resultOutput').innerText = encrypted_result + '\n\nKey: ' + key;
+
+    document.getElementById('resultOutput').innerText = decrypted_result;
 }
 
 function darkMode() {
